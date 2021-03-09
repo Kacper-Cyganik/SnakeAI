34714251726bc26c8622246868dfd31ceaeefdf7
Kacper-Cyganik <67919071+Kacper-Cyganik@users.noreply.github.com>
2021-03-09 17:52:34 +0100

    added steering and timer to SnakeGame
    zmieniony konstruktor w SnakeGame (żeby nie trzeba było uruchamiać z argumentami)

--- src/Snake/SnakeGame.java
+++ src/Snake/SnakeGame.java
@@ -1,34 +1,72 @@
 package Snake;
 
-public class SnakeGame {
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
+public class SnakeGame extends Application {
     private GameGrid grid;
     private Snake snake;
-    private Boolean running;
     private Integer snakeLength;
+    Pane pane;
+    Scene scene;
 
-    SnakeGame(Integer width, Integer height) {
-        grid = new GameGrid(width, height);
+    Snake.Direction direction = Snake.Direction.LEFT; //TODO
+
+    public SnakeGame() {
+        grid = new GameGrid(5,5);
         snakeLength = 2;
     }
 
-    void run() {
-        running = true;
+
+    public void start(Stage primaryStage) throws Exception {
+        //primaryStage.setTitle("Snake");
+        pane = new Pane(); //TODO
+        scene = new Scene(pane, 300, 275); //TODO
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
         snake = new Snake(grid, snakeLength);
-        while (running) {
 
-        }
+        scene.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.RIGHT) {
+                direction = Snake.Direction.RIGHT;
+                System.out.println("PRAWO");
+            } else if (e.getCode() == KeyCode.UP) {
+                direction = Snake.Direction.UP;
+                System.out.println("GORA");
+            } else if (e.getCode() == KeyCode.LEFT) {
+                direction = Snake.Direction.LEFT;
+                System.out.println("LEWO");
+            } else if (e.getCode() == KeyCode.DOWN) {
+                direction = Snake.Direction.DOWN;
+                System.out.println("DOL");
+            }
+        });
+
+        AnimationTimer timer = new AnimationTimer() {
+            long lastTick = 0;
+
+            public void handle(long now) {
+                if (lastTick == 0) {
+                    lastTick = now;
+                    return;
+                }
+                if (now - lastTick > 100000000) {
+                    lastTick = now;
+                    snake.move(direction);
+                }
+
+            }
+        };
+
+        timer.start();
     }
 
-    public Boolean getRunning() {
-        return running;
-    }
-
-    public Integer getSnakeLength() {
-        return snakeLength;
-    }
-
-    public void setSnakeLength(Integer snakeLength) {
-        this.snakeLength = snakeLength;
-    }
-
+    public static void main(String[] args){ launch(args);}
 }
+
